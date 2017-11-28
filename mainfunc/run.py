e0e2feeda170051dacd6e715a8bad477f88ae9da
EKorobov <arhaelk@gmail.com>
2017-11-28 07:40:55 +0200

    trying to import azure.storage.table

--- mainfunc/run.py
+++ mainfunc/run.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python
 
-import os, sys, platform
-
-with open(os.environ['req'], 'r') as inp:
-    print(inp)
+import os
+import json
 
 with open(os.environ['res'], 'w') as res:
-    res.write(platform.python_version())
+    try:
+        from azure.storage.table import TableService, Entity
+    except ImportError as ex:
+        res.write('import error')
+    else:
+        res.write('successfully imported from azure.storage.table')
+    
+with open(os.environ['req'], 'r') as fp:
+    try:
+        print(json.load(fp))
+    except json.JSONDecodeError as ex:
+        print(fp.read())
 
